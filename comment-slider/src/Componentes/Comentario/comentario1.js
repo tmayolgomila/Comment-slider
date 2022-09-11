a0ffc945f5584cc85c9079b9985fa2ec24a701d5
Tomeu Mayol <tmayolgomila@gmail.com>
2022-09-11 13:21:42 +0200

    carta comentario terminada

--- comment-slider/src/Componentes/Comentario/comentario1.js
+++ comment-slider/src/Componentes/Comentario/comentario1.js
@@ -4,13 +4,9 @@ export default function Comentario(){
     return(
     <div className="cartaComentario">
         <div className="titulo">
-        
-        <img className="avatar" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg&fm=jpg"/>
-        <img src="https://images.pexels.com/photos/1202723/pexels-photo-1202723.jpeg?cs=srgb&dl=pexels-sharefaith-1202723.jpg&fm=jpg"/>
-        
-        
-
-        <h4 className="nombreUsuario">Andrea Pereiro</h4>
+        <div className="cajaAvatar"><img className="avatar" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg&fm=jpg"/>
+        <img className="bandera" src="https://images.pexels.com/photos/1202723/pexels-photo-1202723.jpeg?cs=srgb&dl=pexels-sharefaith-1202723.jpg&fm=jpg"/></div>
+        <p className="nombreUsuario">Andrea Pereiro</p>
         </div>
         <p className="comentarioUsuario">La iniciativa de esta página web es emocionante, la descubrí gracias a un amigo y voy a invertir en ella.</p>
     </div>
